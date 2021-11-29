98c58ce9cf45da6f9b9fc968da4c22e7d37e8958
Jakub <jradzik4@gmail.com>
2021-11-29 16:26:21 +0100

    List 4 start

---
+++ Lista4/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "sources/Tree/CTreeDynamic.h"
+
+int main() {
+    vDynamicTest1();
+    return 0;
+}
---
+++ Lista4/sources/Node/CNodeDynamic.h
@@ -0,0 +1,47 @@
+//
+// Created by Jakub Radzik on 29/11/2021.
+//
+
+#ifndef LISTA4_CNODEDYNAMIC_H
+#define LISTA4_CNODEDYNAMIC_H
+
+#include <vector>
+
+class CNodeDynamic {
+public:
+    CNodeDynamic();
+
+    ~CNodeDynamic();
+
+    void vSetValue(int i_new_val);
+
+    int iGetChildrenNumber();
+
+    void vAddNewChild();
+
+    void vAddNewChild(CNodeDynamic *c_node_to_append);
+
+    CNodeDynamic *pcGetChild(int i_child_offset);
+
+    void vPrint();
+
+    void vPrintAllBelow();
+
+    CNodeDynamic *getPcParentNode();
+
+    void setPcParentNode(CNodeDynamic *pc_parent_node);
+
+    void removeChild(CNodeDynamic *pc_child_to_remove);
+
+    int getIVal() const;
+
+    CNodeDynamic *getChild(int i_child_offset);
+
+private:
+    std::vector<CNodeDynamic *> v_children;
+    CNodeDynamic *pc_parent_node;
+    int i_val;
+};
+
+
+#endif //LISTA4_CNODEDYNAMIC_H
